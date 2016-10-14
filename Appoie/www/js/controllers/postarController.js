162dcef8ef12bac0cca31d9d71d3490d767f6f55
alanFreitas <alan_mariano_freitas@hotmail.com>
2016-10-14 20:22:51 -0300

    #8646 - Merge Igor

--- Appoie/www/js/controllers/postarController.js
+++ Appoie/www/js/controllers/postarController.js
@@ -1,17 +1,10 @@
 app.controller('postarController', function($scope, $ionicPopup, $rootScope, $cordovaGeolocation, $cordovaCamera, $state) {
 		
-	$rootScope.publicacao = {};
-
-	$scope.adicionarMarker = function (post){
-
-		$rootScope.publicacao.titulo = post.titulo;
-		$rootScope.publicacao.descricao = post.descricao;
-		$rootScope.publicacao.categoria = "Arborização"; //post.categoria;
-
+	$scope.adicionarMarker = function(){
         $state.go("app.mapa");
     }
 
     
-    
+       
 
 });

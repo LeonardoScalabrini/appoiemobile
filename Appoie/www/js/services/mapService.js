3e3c59ac6380022e1e1c3e6036ecee632b0bbb58
Igor Duarte <Igor Duarte>
2016-10-14 18:37:44 -0300

    #8664 - Previa publicação

--- Appoie/www/js/services/mapService.js
+++ Appoie/www/js/services/mapService.js
@@ -5,4 +5,18 @@ app.service('mapService', function ($http) {
 		return $http.post('/publicacao/salvar', post);
 	}
 
+	this.getMarkers = function ()
+	{
+		return $http.get('publicacao/marcadores');
+	}
+
+	this.getIcons = function ()
+	{
+		return $http.get('publicacao/icones');
+	}
+
+	this.getPostMin = function (id) {
+		return $http.get('publicacao/previa/' + id);
+	}
+
 });

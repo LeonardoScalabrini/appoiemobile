ee7d3e9814aa50b45cd0958369ec6fa87fbb28fb
Igor Duarte <Igor Duarte>
2016-10-22 16:21:08 -0200

    #8247 - IP Service

--- Appoie/www/js/services/loginService.js
+++ Appoie/www/js/services/loginService.js
@@ -3,7 +3,7 @@ app.service('loginService', function ($http) {
 	this.logar = function (usuario)
 	{
 		//return $http.post("http://localhost:8080/usuario/auth", usuario);
-		return $http.post("http://localhost:9092/usuario/auth", usuario);
+		return $http.post("http://192.168.137.235:8080/usuario/auth", usuario);
 	};
 
 	this.recuperarSenha = function ()
